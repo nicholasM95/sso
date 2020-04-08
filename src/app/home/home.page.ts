3a5fa474c570cda1aba8aa8a86d6c8bd9e69f187
Nicholas Meyers <nicholas.meyers@hotmail.com>
2020-04-08 15:40:05 +0200

    last changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MSAdal, AuthenticationContext, AuthenticationResult, AuthenticationSettings } from '@ionic-native/ms-adal/ngx';
-
+declare var Microsoft: any;
 
 @Component({
   selector: 'app-home',
@@ -16,15 +16,17 @@ export class HomePage {
   constructor(private msAdal: MSAdal) {
     this.endpointUrl = 'https://login.microsoftonline.com/009f69a3-375b-4902-b7d6-bb192542da24/wsfed';
     this.azureAdClientId = '0da7e4a5-3068-408c-8e6d-fbd4ea09a53b';
-    this.redirectUri = 'msal0da7e4a5-3068-408c-8e6d-fbd4ea09a53b://auth';
+    // this.redirectUri = 'msal0da7e4a5-3068-408c-8e6d-fbd4ea09a53b://auth';
+    this.redirectUri = 'msauth://be.nicholas.sso1/H%2BmDYxBqqtDxjzX4SxnSVcLMgU0%3D';
     this.login();
   }
 
   login() {
-    /* AuthenticationSettings.setUseBroker(true).then(function() {
-      alert("buh")
-    });*/
-    AuthenticationSettings.INSTANCE.setUseBroker(true);
+    Microsoft.ADAL.AuthenticationSettings.setUseBroker(true).then(function() {
+      alert("use broker true")
+    });
+
+    //AuthenticationSettings.INSTANCE.setUseBroker(true);
     const authContext: AuthenticationContext = this.msAdal.createAuthenticationContext(this.endpointUrl, false);
 
 
